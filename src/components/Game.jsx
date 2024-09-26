47d9f8e7abcc0b19fe5c9c7d82c658b3eb6282b8
namlev <mikipatkowski@protonmail.com>
2024-09-26 11:05:17 +0200

    Fetch and display pokemons

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,3 +1,48 @@
-function Game() {}
+import { useEffect, useState } from "react";
+import api from "../api/api.js";
+import Card from "./Card.jsx";
+
+function Game() {
+  const [items, setItems] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const pokemons = await Promise.all(
+        Object.keys(api).map(async (pokemon) => {
+          const response = await fetch(api[pokemon]);
+          if (!response.ok) {
+            throw new Error(
+              `Error fetching pokemon: ${pokemon}: ${response.status}`,
+            );
+          }
+          const json = await response.json();
+          return {
+            img: json.sprites.other.home.front_default,
+            name: pokemon,
+          };
+        }),
+      );
+      return pokemons;
+    } catch (error) {
+      console.error("Error in fetchData: " + error);
+      return []
+    }
+  };
+
+  useEffect(() => {
+    const effect = async () => {
+      const data = await fetchData();
+      setItems(data);
+    };
+    effect();
+  }, []);
+
+  return (
+      <div className={'game'}>
+          {items.map((item, index) => (
+              <Card key={index} img={item.img} name={item.name} />
+          ))}
+      </div> )
+}
 
 export default Game;
